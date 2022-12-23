a0fc499869e2a5d8c46fec15de322cabaaaee1b0
dhirtiman <dhirtimankhati@gmail.com>
2022-12-23 20:40:19 +0530

    question 2

---
+++ q2.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+void circlepoints(int,int);
+int  main( )
+{
+int x,y,p,r;
+int gdriver=DETECT,gmode;
+initgraph(&gdriver,&gmode,(char*)"");
+printf("Enter the radius");
+scanf("%d",&r);
+x=0;y=r;p=1-r;
+while(x<y)
+{
+x++;
+if(p>0)
+{
+p=p+2*(x-y)+1;
+y--;
+}
+else
+p=p+2*x+1;
+circlepoints(x,y);
+}
+getch();
+closegraph();
+return 0;
+}
+void circlepoints(int x,int y)
+{
+putpixel(x+300,y+300,8);
+putpixel(x+300,-y+300,8);
+putpixel(-x+300,y+300,8);
+putpixel(-x+300,-y+300,8);
+putpixel(y+300,x+300,8);
+putpixel(y+300,-x+300,8);
+putpixel(-y+300,x+300,8);
+putpixel(-y+300,-x+300,8);
+}
