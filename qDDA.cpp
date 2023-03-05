f85c66822386f60938a18221b7601602c0cf9462
Dhirtiman Khati <108656037+dhirtiman@users.noreply.github.com>
2023-03-05 20:22:08 +0530

    Update qDDA.cpp

--- qDDA.cpp
+++ qDDA.cpp
@@ -4,13 +4,21 @@
 int main()
 {
     int gd = DETECT, gm, i;
+
     float x, y, dx, dy, steps;
+
     int x0, x1, y0, y1;
+
     initgraph(&gd, &gm, (char *)"");
-    setbkcolor(WHITE);
-    x0 = 100, y0 = 200, x1 = 500, y1 = 300;
+
+    printf("Enter co-ordinates of first point: ");
+    scanf("%d%d", &x0, &y0);
+    printf("Enter co-ordinates of second point: ");
+    scanf("%d%d", &x1, &y1);
+
     dx = (float)(x1 - x0);
     dy = (float)(y1 - y0);
+
     if (dx >= dy)
     {
         steps = dx;
@@ -19,11 +27,13 @@ int main()
     {
         steps = dy;
     }
+
     dx = dx / steps;
     dy = dy / steps;
     x = x0;
     y = y0;
     i = 1;
+
     while (i <= steps)
     {
         putpixel(x, y, RED);
@@ -31,6 +41,7 @@ int main()
         y += dy;
         i = i + 1;
     }
+    
     getch();
     closegraph();
     return 0;
